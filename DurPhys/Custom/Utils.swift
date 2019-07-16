c217f1b9809d4d2bc8bb605b64d62cc178e5d9b1
Tim Forrer <tim@Matthews-MBP-3.home>
2019-07-16 12:04:07 +0100

    add section to option class

--- DurPhys/Custom/Utils.swift
+++ DurPhys/Custom/Utils.swift
@@ -31,13 +31,15 @@ class Option {
     
     let name: String
     let label: String
+    let section: String?
     let url: String?
     let image: UIImage
     let loginPossible: Bool
     
-    init(name: String, label: String, url: String?, image: UIImage, loginPossible: Bool) {
+    init(name: String, label: String, section:String?, url: String?, image: UIImage, loginPossible: Bool) {
         self.name = name
         self.label = label
+        self.section = section
         self.url = url
         self.image = image
         self.loginPossible = loginPossible
@@ -48,42 +50,49 @@ class Option {
     
         let gettingAround = Option(name: "gettingAround",
         label: "Getting Around",
+        section: nil,
         url: nil,
         image: UIImage(named: "gettingAround")!,
         loginPossible: false)
     
         let courseInformation = Option(name: "courseInformation",
         label: "Course Information (Weekly Timetables)",
+        section: nil,
         url: nil,
         image: UIImage(named: "courseInformation")!,
         loginPossible: false)
     
         let importantContacts = Option(name: "importantContacts",
         label: "Important Contacts",
+        section: nil,
         url: nil,
         image: UIImage(named: "importantContacts")!,
         loginPossible: false)
     
         let teachingFormats = Option(name: "teachingFormats",
         label: "Teaching Formats",
+        section: nil,
         url: nil,
         image: UIImage(named: "teachingFormats")!,
         loginPossible: false)
     
         let physicsPortal = Option(name: "physicsPortal",
         label: "Physics Portal (Homework Hand-In)",
+        section: nil,
         url: "https://teaching.physics.dur.ac.uk",
         image: UIImage(named: "physicsPortal")!,
         loginPossible: true)
     
         let universityEmail = Option(name: "universityEmail",
         label: "University Email",
+        section: nil,
         url: "https://adfs.durham.ac.uk/adfs/ls/?wa=wsignin1.0&wtrealm=urn:federation:MicrosoftOnline&wctx=wa%3Dwsignin1.0%26rpsnv%3D4%26ct%3D1435155873%26rver%3D6.6.6556.0%26wp%3DMBI_SSL%26wreply%3Dhttps:%252F%252Foutlook.office365.com%252Fowa%252F%253Frealm%253Ddurham.ac.uk%2526authRedirect%253Dtrue%26id%3D260563%26whr%3Ddurham.ac.uk%26CBCXT%3Dout",
         image: UIImage(named: "universityEmail")!,
         loginPossible: true)
     
         let duo = Option(name: "duo",
         label: "D.U.O.",
+        section: nil,
         url: "https://duo.dur.ac.uk",
         image: UIImage(named: "duo")!,
         loginPossible: true)
